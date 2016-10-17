3a1bd8c8eeb66883502c26230865fdcf6339d3ea
James T <jtara@tuta.io>
2016-10-17 14:33:39 -0700

    add Gfycat support, & Exception handling

--- utility/download_subreddit_submissions.py
+++ utility/download_subreddit_submissions.py
@@ -4,6 +4,12 @@ import logging
 from .get_subreddit_submissions import GetSubredditSubmissions
 from .general_utility import slugify
 
+# Exceptions
+from downloaders.imgur_downloader.imgurdownloader import (
+    FileExistsException, ImgurException)
+
+
+# downloaders
 from downloaders.direct_link_download import direct_link_download
 from downloaders.imgur_downloader.imgurdownloader import ImgurDownloader
 from downloaders.gfycat.gfycat.gfycat import Gfycat
@@ -38,16 +44,38 @@ class DownloadSubredditSubmissions(GetSubredditSubmissions):
 
             self.log.info('Attempting to save %s as %s' % (url, file_path))
 
-            if url.endswith(media_extensions):
-                direct_link_download(url, file_path)
+            try:
+                if url.endswith(media_extensions):
+                    direct_link_download(url, file_path)
 
-            elif 'imgur.com' in url:
-                imgur = ImgurDownloader(imgur_url=url, dir_download=self.path,
-                    file_name=filename, delete_dne=True, debug=False)
-                imgur.save_images()
+                elif 'imgur.com' in url:
+                    imgur = ImgurDownloader(imgur_url=url, dir_download=self.path,
+                        file_name=filename, delete_dne=True, debug=False)
+                    imgur.save_images()
 
-            elif 'gfycat.com' in url:
-                pass
+                elif 'gfycat.com' in url:
+                    # choose the smallest file on gfycat
+                    gfycat_json = Gfycat().more(url.split("gfycat.com/")[-1]).json()
+                    if gfycat_json["mp4Size"] < gfycat_json["webmSize"]:
+                        urls = [gfycat_json["mp4Url"]]
+                    else:
+                        urls = [gfycat_json["webmUrl"]]
+                    (direct_link_download(url) for url in urls)
 
-            elif 'deviantart.com' in url:
-                download_deviantart_url(url, file_path)
+                elif 'deviantart.com' in url:
+                    download_deviantart_url(url, file_path)
+
+            except (FileExistsException, FileExistsError) as e:
+                msg = '%s already exists (url = %s)' % (file_path, url)
+                self.log.warn(msg)
+                print(msg)
+
+            except ImugrException as e:
+                msg = 'ImgurException: %s' % e.msg
+                self.log.warn(msg)
+                print(msg)
+
+            except Exception as e:
+                msg = 'Exception: %s' % e.msg
+                self.log.warn(msg)
+                print(msg)
