2134f822838fd1d941f822380ceb4f47c347e19d
James T <jtara@tuta.io>
2016-10-21 16:21:22 -0700

    basic func of db manager working

--- database_manager/tpt_database.py
+++ database_manager/tpt_database.py
@@ -1,14 +1,15 @@
 """turbo_palm_tree (tpt) database manager"""
 
 import os, time, sys
-import psycopg2
 import sqlite3
 
 
-
 class TPTDatabaseManager:
+    """`tpt.db` is the database name and `reddit_downloads` is the table name"""
+
 
     def __init__(self):
+        """Create db and table if not created"""
         root_dir = os.path.dirname(__file__)
         db_filename = 'tpt.db'
         db_file_path = os.path.join(root_dir, db_filename)
@@ -18,27 +19,54 @@ class TPTDatabaseManager:
 
         # create table
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS reddit_downloads (
-            id serial PRIMARY KEY,
+            id integer primary key autoincrement,
             subreddit varchar,
             title varchar,
             url varchar,
+            comments_url varchar,
             reddit_id varchar,
-            file_path varchar);""")
+            file_path varchar,
+            dl_time time);""")
 
-        # insert some data
-        self.cursor.execute("INSERT INTO reddit_downloads (subreddit, title)\
-            VALUES ('pics', 'crazy pic title!!!')")
 
-        # write changes to db file
-        self.connection.commit()
+    def insert_data(self, data):
+        """Insert data into table
+        :param data: dictionary with keys of table column names and values to
+            insert
+        """
+        self.cursor.execute("""INSERT INTO reddit_downloads
+            (subreddit, title, url, comments_url, reddit_id, file_path, dl_time)
+            VALUES (:subreddit, :title, :url, :comments_url, :reddit_id,
+                :file_path, :dl_time);""",
+            data)
 
-        # select all, get all
+
+    def print_all(self):
+        """select all, fetch all, print all"""
         self.cursor.execute("SELECT * FROM reddit_downloads")
         rows = self.cursor.fetchall()
-
         for row in rows:
             print(row)
 
 
+    def close(self):
+        """Close & save changes made to database, should be done before
+        program exit
+        """
+        self.connection.commit()
+        self.connection.close()
+
+
 if __name__ == "__main__":
     db = TPTDatabaseManager()
+    my_data = {
+        'subreddit': 'pics',
+        'title': 'This also Japan',
+        'url': 'http://i.imgur.com/z16hW1S.jpg',
+        'comments_url': 'https://www.reddit.com/r/pics/comments/58ocl8/this_also_japan/',
+        'reddit_id': '58ocl8',
+        'file_path': '/home/j/Downloads/walking\ the\ dog.jpg',
+        'dl_time': '10/10/16 13:00'
+    }
+    db.insert_data(my_data)
+    db.close()
