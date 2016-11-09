3f56b070178fbbddc2c2721f726cb425e1117e83
James T <jtara@tuta.io>
2016-11-08 22:38:13 -0800

    [wip] creating pytest methods for testing duplicate image removal

--- tests/test_turbo_palm_tree.py
+++ tests/test_turbo_palm_tree.py
@@ -3,15 +3,34 @@ import pytest
 from turbo_palm_tree.utility.parse_arguments import parse_arguments, SubredditSortTypes
 import os
 import time
+from elasticsearch import Elasticsearch, ElasticsearchException
+from image_match import SignatureES
 
 
 class TestClass:
-
     subreddit_url = "https://www.reddit.com/r/jtaraTest/"
     subreddit_name = "jtaratest"
 
+    @pytest.fixture
+    def get_es(self):
+        """Return the elasticsearch.Elasticsearch object"""
+        es = Elasticsearch()
+        return es
+
+    @pytest.fixture
+    def get_im(self, get_es):
+        """Return the image_match.SignatureES object (requires Elasticsearch object to instantiate)"""
+        ses = SignatureES(get_es, distance_cutoff=0.40)
+        return ses
+
+    @pytest.fixture
+    def get_es_and_im(self, get_es, get_im):
+        """Return es object & SignatureES object"""
+        return get_es, get_im
+
     def get_random_parsed_arg(self):
         """Returns a parsed arg with a random sort type"""
+
         def get_random_sort_type():
             """Return a random sort types drawing from the enum class of all possible sort types"""
             sort_types = list(st.value for st in SubredditSortTypes)
@@ -29,3 +48,17 @@ class TestClass:
         assert sort_type == args.sort_type
         assert args.subreddit == sr_name
         assert path == args.directory
+
+    def test_elasticsearch(self, get_es):
+        es = get_es
+        try:
+            for i in range(5):
+                es.ping()
+        except ElasticsearchException:
+            assert False
+
+    def test_image_match(self):
+        pass
+
+    def test_duplicate_removal(self):
+        pass
