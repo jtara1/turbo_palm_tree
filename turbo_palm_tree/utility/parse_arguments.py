50f18756d00f885bec32ba6e306c5446eca7e734
James T <jtara@tuta.io>
2018-02-09 10:14:33 -0800

    fix: help message in argparse setup (close #7)

--- turbo_palm_tree/utility/parse_arguments.py
+++ turbo_palm_tree/utility/parse_arguments.py
@@ -49,8 +49,8 @@ def parse_arguments(args):
                             joined with name of subreddit')
     parser.add_argument('--sort-type', '-s', metavar='<s>', required=False,
                         default='hot', type=str,
-                        help='Sort type for subreddit. See '
-                             'https://github.com/jtara1/turbo_palm_tree/search?utf8=%E2%9C%93&q=SubredditSortTypes')
+                        help='Sort type for subreddit. Search '
+                             'SubredditSortTypes in this repo for more info')
     parser.add_argument('--limit', '--num', '-l', metavar='<l>', required=False,
                         default=5, type=int, help='Number of submissions to'
                         ' download from; defaults to 5')
